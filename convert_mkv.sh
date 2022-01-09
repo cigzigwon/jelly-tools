20c15dc2acba932eb95ce2850c60548d9068ce76
Chris Sobeck <csobeck@pyroclasti.cloud>
2022-01-08 21:07:35 -0500

    Seems to be workign now

--- convert_mkv.sh
+++ convert_mkv.sh
@@ -1,10 +1,64 @@
 #!/bin/bash
 
+usage()
+{
+cat << EOF
+usage: $0 PARAM [-f|--format] [-t|--tag] [-h|--help]
+
+This script unzips/converts media (.mkv) in bulk via ffmpeg and HandbrakeCLI.
+
+OPTIONS:
+   PARAM        The video source name to find
+   -h|--help    Show this message
+   -f|--format  Override Handbrake preset format: Android 720p30
+   -t|--tag  		Tag the media to move to Jellyfin media dir
+EOF
+}
+
+PARAM=$1; shift
 DEST=~/shared
 SRC=~/Downloads
 FORMAT="Android 720p30"
+TAG="movies"
 
-for f in ${@}; do
+if [ "$PARAM" == -h ]; then
+	usage
+	exit
+fi
+
+while [ ! $# -eq 0 ]; do
+    case "$1" in
+        -f | --format)
+            if [ "$2" ]; then
+                FORMAT=$2
+                shift
+            else
+                echo '--format requires a preset format string'
+                exit 1
+            fi
+            ;;
+        -t | --tag)
+            if [ "$2" ]; then
+                TAG=$2
+                shift
+            else
+                echo '--tag requires a name'
+                exit 1
+            fi
+            ;;
+        -h | --help)
+            usage
+            exit
+            ;;
+        *)
+            usage
+            exit
+            ;;
+    esac
+    shift
+done
+
+for f in ${PARAM}; do
 	# recursive find to exec unrar on files @ is search filter string
 	find "$SRC" -type f -name "*${f}*.rar" -exec unrar e {} $DEST \;
 
@@ -30,5 +84,7 @@ for f in *.mkv; do
 	sleep 4
 	rm -f "$f"
 	curl --insecure -T "$outfile" --user 'chris:$n00ker36' ftp://192.168.1.9
-	mv -f "$outfile" /media/chris/
+
+	# move to Jellyfin media dir
+	mv -f "$outfile" "/media/chris/${TAG}"
 done
