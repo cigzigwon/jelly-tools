e550dd349eee9822419db757a10e06ae4b6ba72c
Chris Sobeck <csobeck@pyroclasti.cloud>
2022-01-15 22:22:36 -0500

    Can pull the TV series ID from IMDB

--- jellyfin_tools/lib/jellyfin_tools.ex
+++ jellyfin_tools/lib/jellyfin_tools.ex
@@ -1,4 +1,7 @@
 defmodule JellyfinTools do
+
+  @imdb_apikey "k_q3758gy3"
+
   @moduledoc """
   Documentation for `JellyfinTools`.
   """
@@ -14,5 +17,28 @@ defmodule JellyfinTools do
   """
   def hello do
     :world
+  end
+
+  def list_shared() do
+    File.ls!("/root/media/")
+  end
+
+  def search_series(name) do
+    url = "https://imdb-api.com/en/API/SearchSeries/#{@imdb_apikey}/#{name}"
+      |> URI.encode()
+
+    resp =
+      case HTTPoison.get(url) do
+        {:ok, resp} ->
+          # IO.puts resp.body
+          Poison.decode!(resp.body)
+        {:error, %HTTPoison.Error{reason: reason}} ->
+          IO.inspect reason
+      end
+
+    if resp["results"] |> length() do
+      top = resp["results"] |> Enum.fetch!(0)
+      top["id"]
+    end
   end
 end
